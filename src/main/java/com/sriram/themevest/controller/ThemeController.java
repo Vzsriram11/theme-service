a502eeb9da73276423c0861f1d323016d25c0704
Sriram <sriram11@gmail.com>
2026-07-01 23:07:49 -0500

    Added Swagger API documentation

--- src/main/java/com/sriram/themevest/controller/ThemeController.java
+++ src/main/java/com/sriram/themevest/controller/ThemeController.java
@@ -3,6 +3,8 @@ package com.sriram.themevest.controller;
 import com.sriram.themevest.dto.CreateThemeRequest;
 import com.sriram.themevest.entity.Theme;
 import com.sriram.themevest.service.ThemeService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -12,27 +14,35 @@ import java.util.List;
 @RestController
 @RequestMapping("/themes")
 @RequiredArgsConstructor
+@Tag(
+        name = "Theme API",
+        description = "APIs for managing investment themes"
+)
 public class ThemeController {
 
     private final ThemeService themeService;
 
     @GetMapping
+    @Operation(summary = "Retrieve all investment themes")
     public List<Theme> getAllThemes() {
         return themeService.getAllThemes();
     }
 
     @GetMapping("/{id}")
+    @Operation(summary =  "Get a specific Theme based on ThemeId")
     public Theme getTheme(@PathVariable Long id) {
         return themeService.getTheme(id);
     }
 
     @PostMapping
+    @Operation(summary =  "create a specific Theme")
     public Theme createTheme(
             @Valid @RequestBody CreateThemeRequest request) {
 
         return themeService.createTheme(request);
     }
     @PutMapping("/{id}")
+    @Operation(summary = "Update an existing investment theme")
     public Theme updateTheme(
             @PathVariable Long id,
             @Valid @RequestBody CreateThemeRequest request) {
@@ -41,6 +51,7 @@ public class ThemeController {
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Delete a theme")
     public void deleteTheme(@PathVariable Long id) {
         themeService.deleteTheme(id);
     }
